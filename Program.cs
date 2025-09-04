9dcf6033b74a5cb280b9ade2b4845941b838a22a
t343s <t343s@PC-01>
2025-09-04 14:54:37 +0300

    добавил DI

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace ConsoleCS_ProjectManagementSystem
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleCS_ProjectManagementSystem
 {
     internal class Program
     {
+        private static ServiceProvider _serviceProvider;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IServiceCollection services = new ServiceCollection();
+            //AddServices(services);
+            //_serviceProvider = services.BuildServiceProvider();
+            //ProgramStartup();
         }
+
+
+        //private static void ProgramStartup()
+        //{
+        //    _serviceProvider.GetService<MainPage>();
+        //}
+
+        //private static void AddServices(IServiceCollection services)
+        //{
+        //    services.AddTransient<MainPage>();
+        //    services.AddSingleton<INavigationBetweenPages, NavigationBetweenPagesService>();
+        //}
     }
 }
