8aa444d739fec4d28ea5f3d890c5f630820f11a6
t343s <t343s@PC-01>
2025-09-04 14:49:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleCS_ProjectManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
