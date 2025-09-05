3ece7e6a9700e59c97152c8143360be1bd98593a
t343s <t343s@PC-01>
2025-09-05 23:51:21 +0300

    добавил интерфейсы

---
+++ Model/Interfaces/IRole.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleCS_ProjectManagementSystem.Infrastructure.Enums;
+
+namespace ConsoleCS_ProjectManagementSystem.Model.Interfaces
+{
+    public interface IRole
+    {
+        string Name { get; set; }
+        RolesEnum Rights { get; set; }
+    }
+}
---
+++ Model/Interfaces/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleCS_ProjectManagementSystem.Model.Interfaces
+{
+    public interface IUser
+    {
+        string Name { get; set; }
+        string Password { get; set; }
+        IRole role { get; set; }
+    }
+}
