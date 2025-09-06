ca7589f216d02ead115084339bf48d46bbfa2219
t343s <t343s@PC-01>
2025-09-06 23:11:54 +0300

    начал реализовывать     PageNavigationService

---
+++ Infrastructure/Interfaces/IPageNavigation.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleCS_ProjectManagementSystem.Pages.Base;
+
+namespace ConsoleCS_ProjectManagementSystem.Infrastructure.Interfaces
+{
+    public interface IPageNavigation
+    {
+        public void Back();
+        public void Open(BasePage page);
+    }
+}
---
+++ Infrastructure/Services/PageNavigationService.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleCS_ProjectManagementSystem.Infrastructure.Interfaces;
+using ConsoleCS_ProjectManagementSystem.Pages.Base;
+
+namespace ConsoleCS_ProjectManagementSystem.Infrastructure.Services
+{
+    public class PageNavigationService : IPageNavigation
+    {
+        public void Back()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open(BasePage page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
