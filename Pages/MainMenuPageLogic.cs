cf3ed69b0bf9629a5dbbe732cf1d4125358b8fcb
t343s <t343s@PC-01>
2025-09-06 19:36:48 +0300

    удалил билдер и создал комманды

--- Pages/MainMenuPageLogic.cs
+++ Pages/MainMenuPageLogic.cs
@@ -12,23 +12,38 @@ namespace ConsoleCS_ProjectManagementSystem.Pages
                 [ new MenuElement(){
                     Id = 1,
                     Name = "Добавить пользователя",
-                    Execute = (object? obj) => { }
+                    Execute = OpenAddUser
                 }] = RightsEnum.CreateUser,
                 [new MenuElement()
                 {
                     Id = 2,
                     Name = "Посмотреть задачи",
-                    Execute = (object? obj) => { }
+                    Execute = OpenTaskViewer
                 }] = RightsEnum.None,
                 [new MenuElement()
                 {
                     Id = 3,
                     Name = "Выход",
-                    Execute = (object? obj) => { }
+                    Execute = OpenExit
                 }] = RightsEnum.None,
             };
         }
 
+        #region Commands
+        private void OpenExit(object? obj)
+        {
+            Environment.Exit(0);
+        }
 
+        private void OpenTaskViewer(object? obj)
+        {
+            Environment.Exit(0);
+        }
+
+        private void OpenAddUser(object? obj)
+        {
+            Environment.Exit(0);
+        }
+        #endregion
     }
 }
