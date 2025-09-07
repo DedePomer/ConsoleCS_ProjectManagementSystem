7ed718b871ac5d2c3724ba0d08fd45ae1b5e7f79
t343s <t343s@PC-01>
2025-09-08 02:49:50 +0300

    Доделал AddUserPage

--- Pages/AddUserPage.cs
+++ Pages/AddUserPage.cs
@@ -30,38 +30,8 @@ namespace ConsoleCS_ProjectManagementSystem.Pages
         }
         public override void Open()
         {
-            List<DefaultRole> roles = _userCreationService
-                .GetRoles()
-                .ToList();
-            var RolesNames = roles
-                .Select(x => x.Name)
-                .ToList();
-
-            while (true)
-            {
-                ShowDisplayElements(RolesNames, "Выберите роль нового пользователя\n");
-                NavigationLoopService loopService = new NavigationLoopService(RolesNames, GetCountStrokeInTitle());
-                int roleId = loopService.GetNumberSelectedElement(false);
-
-                ShowElementsForInputs(_elements, "Введите логин и пароль нового пользователя\n");
-                loopService = new NavigationLoopService(RolesNames, GetCountStrokeInTitle());
-
-                DefaultUser newUser = new DefaultUser();
-                newUser.Name = _elements["Логин"];
-                newUser.Password = _elements["Пароль"];
-                newUser.Role = roles[roleId];
-                if (!_userCreationService.UserExist(newUser.Name))
-                {
-                    _userCreationService.CreateUser(newUser);
-                    break;                   
-                }
-                else
-                {
-                    ShowException("Такой пользователь существует");
-                }
-            }
+            CreateNewUser();
             _navigation.Back();
-
         }
     }
 }
--- Pages/AddUserPageLogic.cs
+++ Pages/AddUserPageLogic.cs
@@ -1,4 +1,7 @@
-﻿namespace ConsoleCS_ProjectManagementSystem.Pages
+﻿using ConsoleCS_ProjectManagementSystem.Infrastructure.Services;
+using ConsoleCS_ProjectManagementSystem.Model.DataType;
+
+namespace ConsoleCS_ProjectManagementSystem.Pages
 {
     public partial class AddUserPage
     {
@@ -6,6 +9,48 @@
         {
             _elements.Add("Логин","");
             _elements.Add("Пароль", "");
+        }
+
+        private void CreateNewUser()
+        {
+            List<DefaultRole> roles = _userCreationService
+                .GetRoles()
+                .ToList();
+            var rolesNames = roles
+                .Select(x => x.Name)
+                .ToList();
+
+            while (true)
+            {
+                int roleId = GetSelectedItem(rolesNames);
+
+                DefaultUser newUser = new DefaultUser();
+                newUser.Name = _elements["Логин"];
+                newUser.Password = _elements["Пароль"];
+                newUser.Role = roles[roleId];
+
+                if (!_userCreationService.UserExist(newUser.Name))
+                {
+                    _userCreationService.CreateUser(newUser);
+                    break;
+                }
+                else
+                {
+                    ShowException("Такой пользователь существует");
+                }
+            }
+        }
+
+        private int GetSelectedItem(List<string> rolesNames)
+        {
+            ShowDisplayElements(rolesNames, "Выберите роль нового пользователя\n");
+            NavigationLoopService loopService = new NavigationLoopService(rolesNames, GetCountStrokeInTitle());
+            int selectedItem = loopService.GetNumberSelectedElement(false);
+
+            ShowElementsForInputs(_elements, "Введите логин и пароль нового пользователя\n");
+            loopService = new NavigationLoopService(rolesNames, GetCountStrokeInTitle());
+
+            return selectedItem;
         }
     }
 }
