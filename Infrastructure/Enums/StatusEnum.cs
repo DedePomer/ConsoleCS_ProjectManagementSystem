3597fd72fb59ea25d069f35775c9b3e4fa9ffd3b
t343s <t343s@PC-01>
2025-09-08 03:16:47 +0300

    добавил StatusEnum

---
+++ Infrastructure/Enums/StatusEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleCS_ProjectManagementSystem.Infrastructure.Enums
+{
+    public enum StatusEnum
+    {
+        None = 0,
+        Done = 1,
+        ToDo = 2,
+        InProgress = 3
+    }
+}
---
+++ Model/DataType/TaskElement.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleCS_ProjectManagementSystem.Model.Interfaces;
+
+namespace ConsoleCS_ProjectManagementSystem.Model.DataType
+{
+    public class TaskElement : IElement
+    {
+        public int Id { get ; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+
+        public Action<object?> Execute { get; set; }
+    }
+}
