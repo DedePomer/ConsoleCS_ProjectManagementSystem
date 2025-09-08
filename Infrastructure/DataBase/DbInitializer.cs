af2d18b980115639c3e5e7883bac686e34808c00
t343s <t343s@PC-01>
2025-09-08 05:14:41 +0300

    DbInitializer заработал

--- Infrastructure/DataBase/DbInitializer.cs
+++ Infrastructure/DataBase/DbInitializer.cs
@@ -83,11 +83,19 @@ namespace ConsoleCS_ProjectManagementSystem.Infrastructure.DataBase
             {
                 const string adminPassword = "admin";
 
+                connection.Execute("""
+                
+                INSERT INTO Users (id, name)
+                VALUES  
+                (0,'defaultUser')           
+                
+                """);
+
+
                 connection.Execute(new CommandDefinition("""
                 
                 INSERT INTO Users (id, name, password, roleid)
                 VALUES  
-                (0,'defaultUser',,)  
                 (1,'admin', @Password, 1)            
                 
                 """, new { Password = HashService.GetHash(adminPassword) }));
