8bdfac7a82e8d82e08facaa08fdc1a9acbc6b31d
t343s <t343s@PC-01>
2025-09-08 06:57:27 +0300

    add TaskChangePage

---
+++ Pages/TaskChangePage.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleCS_ProjectManagementSystem.Infrastructure.DataBase;
+using ConsoleCS_ProjectManagementSystem.Infrastructure.Enums;
+using ConsoleCS_ProjectManagementSystem.Infrastructure.Interfaces;
+using ConsoleCS_ProjectManagementSystem.Model.DataType;
+using ConsoleCS_ProjectManagementSystem.Pages.Base;
+
+namespace ConsoleCS_ProjectManagementSystem.Pages
+{
+    public partial class TaskChangePage : BasePage
+    {
+        private readonly DefaultUser _user;
+        private readonly IDbConnectionFactory _connectionFactory;
+        private readonly IPageNavigation _navigation;
+
+        private Dictionary<MenuElement, RightsEnum> _elements = new Dictionary<MenuElement, RightsEnum>();
+
+        public TaskChangePage(DefaultUser user, IDbConnectionFactory connectionFactory, IPageNavigation navigation)
+        {
+            _user = user;
+            _connectionFactory = connectionFactory;
+            _navigation = navigation;
+
+            _navigation.Add(this);
+
+            FillDictionary();
+        }
+
+
+        public override void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
